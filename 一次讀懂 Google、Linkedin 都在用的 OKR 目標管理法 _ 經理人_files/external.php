7ce66f7197a110ee6abec83516e0d3a892664d36
jash-git <jash.liao@gmail.com>
2018-10-02 10:45:24 +0800

    OKR modified

--- 一次讀懂 Google、Linkedin 都在用的 OKR 目標管理法 _ 經理人_files/external.php
+++
@@ -1,8 +0,0 @@
-/* out-freq, hit 3 tw */
-/* silent, no pid */
-if (ONEAD_get_response[0] !== undefined && typeof ONEAD_get_response[0] == 'function'){
-    ONEAD_get_response[0](null);
-}
-
-/* server */
-
